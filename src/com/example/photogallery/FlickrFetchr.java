ea28c676f4ccd35cad7d758acfa6cd13fce1ecfb
Justin Cuaresma <justincuaresma@gmail.com>
2014-12-02 14:13:35 -0500

    Added Hook for Flickr API
    Added:
    + Flickr REST implementation

--- src/com/example/photogallery/FlickrFetchr.java
+++ src/com/example/photogallery/FlickrFetchr.java
@@ -1,12 +1,23 @@
 package com.example.photogallery;
 
 import java.io.ByteArrayOutputStream;
+import java.io.Console;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+import android.net.Uri;
+import android.util.Log;
+
 public class FlickrFetchr {
+	public static final String TAG = "FlickrFetchr";
+	
+	private static final String ENDPOINT = "https://api.flickr.com/services/rest/";
+	private static final String API_KEY = "5bb3db600c186f0cce32bcf72b8db715";
+	private static final String METHOD_GET_RECENT = "flickr.photos.getRecent";
+	private static final String PARAM_EXTRAS = "extras";
+	private static final String EXTRA_SMALL_URL = "url_s";
 	
 	byte[] getUrlBytes(String urlSpec) throws IOException {
 		URL url = new URL(urlSpec);
@@ -37,6 +48,23 @@ public class FlickrFetchr {
 	
 	public String getUrl(String urlSpec) throws IOException {
 		return new String(getUrlBytes(urlSpec));
+	}
+	
+	public void fetchItems() {
+		try {
+			// Uri.Builder is a convenience class for creating properly escaped paramterized URLs
+			String url = Uri.parse(ENDPOINT).buildUpon()
+					.appendQueryParameter("method", METHOD_GET_RECENT)
+					.appendQueryParameter("api_key", API_KEY)
+					.appendQueryParameter(PARAM_EXTRAS, EXTRA_SMALL_URL)
+					.build().toString();
+			System.out.println("url:\t" + url.toString());
+			String xmlString = getUrl(url.toString());
+			
+			Log.i(TAG, "Received xml: " + xmlString);
+		} catch (IOException io) {
+			Log.e(TAG, "Failed to fetch items", io);
+		}
 	}
 
 }
