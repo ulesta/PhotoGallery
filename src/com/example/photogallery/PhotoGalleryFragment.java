19fec4e2ef9c5a7edf2085b16e808909b753b78d
Justin Cuaresma <justincuaresma@gmail.com>
2014-12-05 11:11:32 -0500

    Integrated AsyncTask with Main Thread to display fetched content. Added Indeterminate Loading
    Added:
    - Overridden PostExecute(..) to perform actions on Main Thread
    - Added faux-loading screen

--- src/com/example/photogallery/PhotoGalleryFragment.java
+++ src/com/example/photogallery/PhotoGalleryFragment.java
@@ -1,20 +1,26 @@
 package com.example.photogallery;
 
-import java.io.IOException;
+import java.util.ArrayList;
 
+import model.GalleryItem;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.FrameLayout;
 import android.widget.GridView;
 
 public class PhotoGalleryFragment extends Fragment {
 	private static final String TAG = "PhotoGalleryFragment";
+	
 	GridView mGridView;
+	FrameLayout mLoading;
+	
+	ArrayList<GalleryItem> mItems;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -31,15 +37,61 @@ public class PhotoGalleryFragment extends Fragment {
 		View v = inflater.inflate(R.layout.fragment_photo_gallery, container, false);
 		
 		mGridView = (GridView) v.findViewById(R.id.gridView);
+		mLoading = (FrameLayout) v.findViewById(R.id.loadingPanel);
+		
+		setupAdapter();
 		
 		return v;
 	}
 	
-	private class FetchItemsTask extends AsyncTask<Void, Void, Void> {
+	/* Third param in generic is the type of result produced by AsyncTask, it sets the value
+	 * returned by AsyncTask. 
+	 * 
+	 * First generic param: specifies input parameters i.e. doInBackground(String...).
+	 * 
+	 * Second generic param: specify type for sending progress updates
+	 * 
+	 *  */
+	private class FetchItemsTask extends AsyncTask<Void, Void, ArrayList<GalleryItem>> {
+		/* Note: do not update UI on background thread, memory corruption -> unsafe
+		 */
 		@Override
-		protected Void doInBackground(Void... params) {
-			new FlickrFetchr().fetchItems();
-			return null;
+		protected ArrayList<GalleryItem> doInBackground(Void... params) {
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			return new FlickrFetchr().fetchItems();
+		}
+
+		/* Happens after doInBackground is complete, also executed on main thread hence safe
+		 * to update UI in it. Accepts the list fetched insdie doInBackground(..)
+		 */
+		@Override
+		protected void onPostExecute(ArrayList<GalleryItem> items) {
+			mLoading.setVisibility(View.INVISIBLE);
+			mItems = items;
+			setupAdapter();
+		}
+		
+		
+		
+	}
+	
+	private void setupAdapter() {
+		/* First do checks -- recall: fragments can exist unattached from any activity.
+		 * If out fragment is not attached, operations that rely on that activity like creating
+		 * our own ArrayAdapter will fail
+		 */
+		if (getActivity() == null || mGridView == null) {
+			return;
+		}
+		
+		if (mItems != null) {
+			mGridView.setAdapter(new ArrayAdapter<GalleryItem>(getActivity(), android.R.layout.simple_gallery_item, mItems));
+		} else {
+			mGridView.setAdapter(null);
 		}
 		
 	}
